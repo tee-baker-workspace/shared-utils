311d8f82c900fa7769e5266527562154b642e0db
Huzaifa Arshad <huzaifaarshad@Huzaifas-MacBook-Pro-2.local>
2024-12-28 02:00:18 +0500

    Added generic types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,14 @@
 import { GenderOption } from "./lib/types";
 
-export * from "./types";
 export function isEmptyOrNil(value: any): boolean;
 export function generateRandomUsername(email: string): string;
 export const GENDERS: GenderOption[];
+export { IGender, GenderOption, IUser } from "./lib/types/user";
+export {
+  IRegisterUser,
+  ILoginUser,
+  IAccessTokenResponse,
+  IAuthSessionResponse,
+  IUpdateUserPayload,
+} from "./lib/types/auth";
+export { GenericObject } from "./lib/types/generic";
