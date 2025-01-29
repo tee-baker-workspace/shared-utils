b7168170c3b983a4ca9e82e6c3da4ef77630a1d3
Huzaifa Arshad <huzaifaarshad@Huzaifas-MacBook-Pro-2.local>
2025-01-30 01:14:55 +0500

    Added firstOrNull helper

--- src/index.ts
+++ src/index.ts
@@ -23,4 +23,8 @@ export {
 } from './lib/enums';
 
 export { GENDERS } from './lib/utils/constants';
-export { generateRandomUsername, isEmptyOrNil } from './lib/utils/helpers';
+export {
+  generateRandomUsername,
+  isEmptyOrNil,
+  firstOrNull,
+} from './lib/utils/helpers';
--- src/lib/utils/helpers.ts
+++ src/lib/utils/helpers.ts
@@ -1,7 +1,9 @@
-import { anyPass, isEmpty, isNil } from 'ramda';
+import { anyPass, isEmpty, isNil, propOr } from 'ramda';
 import { generateFromEmail } from 'unique-username-generator';
 
 export const isEmptyOrNil = anyPass([isEmpty, isNil]);
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const firstOrNull = propOr(null, 0 as any);
 
 export const generateRandomUsername = (email: string) => {
   const username = generateFromEmail(email, 3);
